7d3be470dcf9920f88ec4bb2a9e3ec560f2ff7d0
littlepig2013 <zhuzichen19960417@gmail.com>
2024-03-20 05:59:00 -0400

    update script

--- exp_kvbench_ingestion_plain_tput.sh
+++ exp_kvbench_ingestion_plain_tput.sh
@@ -7,7 +7,7 @@ E=1024
 R=1
 bpk=8
 BCC=16384
-DB_HOME="/scratchNVM1/zczhu/test_db_dir/db_working_home"
+DB_HOME="./db_working_home"
 
 throughput_interval=10000
 D_LIST=("100000")
@@ -18,11 +18,11 @@ mkdir -p "output/"
 for D in ${D_LIST[@]}
 do
 	U=`echo "1000000-${D}" | bc`
-	echo "./plain_benchmark -T ${T} -E ${E} --dd -p ${DB_HOME} --iwp /scratchHDDa/zczhu/K-V-Workload-Generator/ingestion_workload.txt --qwp /scratchHDDa/zczhu/K-V-Workload-Generator/kvbench_D${D}_U${U}_ingestion_workload.txt -B ${B} -P 4096 -b ${bpk} --BCC ${BCC} -V 1 --dw --dr --clct-tputi ${throughput_interval} > output/kvbench_D${D}_U${U}_interleaved_output.txt"
-	./plain_benchmark -T ${T} -E ${E} --dd -p ${DB_HOME} --iwp /scratchHDDa/zczhu/K-V-Workload-Generator/ingestion_workload.txt --qwp /scratchHDDa/zczhu/K-V-Workload-Generator/kvbench_D${D}_U${U}_ingestion_workload.txt -B ${B} -P 4096 -b ${bpk} --BCC ${BCC} -R ${R} -V 1 --dw --dr --clct-tputi ${throughput_interval} > output/kvbench_D${D}_U${U}_interleaved_output.txt
+	echo "./plain_benchmark -T ${T} -E ${E} --dd -p ${DB_HOME} --iwp ../K-V-Workload-Generator/ingestion_workload.txt --qwp ../K-V-Workload-Generator/kvbench_D${D}_U${U}_ingestion_workload.txt -B ${B} -P 4096 -b ${bpk} --BCC ${BCC} -V 1 --dw --dr --clct-tputi ${throughput_interval} > output/kvbench_D${D}_U${U}_interleaved_output.txt"
+	./plain_benchmark -T ${T} -E ${E} --dd -p ${DB_HOME} --iwp ../K-V-Workload-Generator/ingestion_workload.txt --qwp ../K-V-Workload-Generator/kvbench_D${D}_U${U}_ingestion_workload.txt -B ${B} -P 4096 -b ${bpk} --BCC ${BCC} -R ${R} -V 1 --dw --dr --clct-tputi ${throughput_interval} > output/kvbench_D${D}_U${U}_interleaved_output.txt
 	mv throughputs.txt output/kvbench_D${D}_U${U}_interleaved_throughputs.txt
 	rm ${DB_HOME}/*
-	cd "/scratchHDDa/zczhu/K-V-Workload-Generator/"
+	cd "../K-V-Workload-Generator/"
 	grep "^U" kvbench_D${D}_U${U}_ingestion_workload.txt > kvbench_D${D}_U${U}_sequential_ingestion_workload.txt
 	grep "^D" kvbench_D${D}_U${U}_ingestion_workload.txt >> kvbench_D${D}_U${U}_sequential_ingestion_workload.txt
 	grep "^Q" kvbench_D${D}_U${U}_ingestion_workload.txt >> kvbench_D${D}_U${U}_sequential_ingestion_workload.txt
--- exp_kvbench_plain.sh
+++ exp_kvbench_plain.sh
@@ -6,7 +6,7 @@ T=4
 E=1024
 R=1
 BCC=16384
-DB_HOME="/scratchNVM1/zczhu/test_db_dir/db_working_home"
+DB_HOME="./db_working_home"
 
 Z_list=("0.0" "0.1" "0.2" "0.3" "0.4" "0.5" "0.6" "0.7" "0.8" "0.9" "1.0")
 ZD_list=("0" "3")
@@ -18,8 +18,8 @@ for ZD in ${ZD_list[@]}
 do
 	for Z in ${Z_list[@]}
 	do
-		echo "./plain_benchmark -T ${T} -E ${E} --dd -p ${DB_HOME} --iwp /scratchHDDa/zczhu/K-V-Workload-Generator/ingestion_workload.txt --qwp /scratchHDDa/zczhu/K-V-Workload-Generator/kvbench_Z${Z}_ZD${ZD}_query_workload.txt -B ${B} -P 4096 --BCC ${BCC} -V 1 --dw --dr > output/kvbench_Z${Z}_ZD${ZD}_output.txt"
-		./plain_benchmark -T ${T} -E ${E} --dd -p ${DB_HOME} --iwp /scratchHDDa/zczhu/K-V-Workload-Generator/ingestion_workload.txt --qwp /scratchHDDa/zczhu/K-V-Workload-Generator/kvbench_Z${Z}_ZD${ZD}_query_workload.txt -B ${B} -P 4096 --BCC ${BCC} -R ${R} -V 1 --dw --dr > output/kvbench_Z${Z}_ZD${ZD}_output.txt
+		echo "./plain_benchmark -T ${T} -E ${E} --dd -p ${DB_HOME} --iwp ../K-V-Workload-Generator/ingestion_workload.txt --qwp ../K-V-Workload-Generator/kvbench_Z${Z}_ZD${ZD}_query_workload.txt -B ${B} -P 4096 --BCC ${BCC} -V 1 --dw --dr > output/kvbench_Z${Z}_ZD${ZD}_output.txt"
+		./plain_benchmark -T ${T} -E ${E} --dd -p ${DB_HOME} --iwp ../K-V-Workload-Generator/ingestion_workload.txt --qwp ../K-V-Workload-Generator/kvbench_Z${Z}_ZD${ZD}_query_workload.txt -B ${B} -P 4096 --BCC ${BCC} -R ${R} -V 1 --dw --dr > output/kvbench_Z${Z}_ZD${ZD}_output.txt
 		rm ${DB_HOME}/*
 	done
 done
